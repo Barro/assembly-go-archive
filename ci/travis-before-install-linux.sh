7212306ca9d5044001808a3e7a0f723b2526d14d
Jussi Judin <jjudin+assembly-archive@iki.fi>
2019-03-04 00:01:58 +0200

    Check Bazel checksum before install

--- ci/travis-before-install-linux.sh
+++ ci/travis-before-install-linux.sh
@@ -4,8 +4,11 @@ set -xeuo pipefail
 
 BAZEL_VERSION=0.23.0
 BAZEL_ARCHIVE=bazel_"$BAZEL_VERSION"-linux-x86_64.deb
+BAZEL_SHA256=6d3f5a2eae9021671a967c362513eaaae979b76e9c725359cb75ee149a92817e
+echo "$BAZEL_SHA256 *$BAZEL_ARCHIVE" > checksums.sha256
 
 curl --location --retry 5 -o "$BAZEL_ARCHIVE" \
      https://github.com/bazelbuild/bazel/releases/download/"$BAZEL_VERSION"/"$BAZEL_ARCHIVE"
+sha256sum -c checksums.sha256
 
 sudo dpkg -i "$BAZEL_ARCHIVE"
