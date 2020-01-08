ac9d1005c1a61fb9ec15fcc56dad49d022e2e721
Jussi Judin <jjudin+assembly-archive@iki.fi>
2020-01-08 22:17:43 +0200

    Add support for multiple target platforms in Gitlab

--- ci/gitlab-ci.sh
+++ ci/gitlab-ci.sh
@@ -10,12 +10,21 @@ apt-get update
 apt-get install -y -t bionic zopfli
 
 bazel version
-bazel build ...:all
-cp bazel-bin/assembly-archive-pkg.tar .
+# Enable more platforms if the need arises to distribute them::
+TARGET_PLATFORMS=(
+    # darwin_amd64
+    linux_amd64
+    # linux_arm
+    # windows_amd64
+)
+for platform in "${TARGET_PLATFORMS[@]}"; do
+    bazel build ...:all --platforms=@io_bazel_rules_go//go/toolchain:"$platform"
+    cp bazel-bin/assembly-archive-pkg.tar assembly-archive-"$platforms".tar
 
-if [[ -f $GITLAB_RELEASES_KEY_PRIVATE_FILE ]]; then
-    bazel run :sign-pkg "$GITLAB_RELEASES_KEY_PRIVATE_FILE"
-    cp bazel-bin/assembly-archive-pkg.tar.sig .
-else
-    echo "NO-SIGNATURE-IN-UNPROTECTED-BRANCH" > assembly-archive-pkg.tar.sig
-fi
+    if [[ -f $GITLAB_RELEASES_KEY_PRIVATE_FILE ]]; then
+        bazel run :sign-pkg "$GITLAB_RELEASES_KEY_PRIVATE_FILE"
+        cp bazel-bin/assembly-archive-pkg.tar.sig assembly-archive-"$platforms".tar.sig
+    else
+        echo "NO-SIGNATURE-IN-UNPROTECTED-BRANCH" > assembly-archive-"$platform".tar.sig
+    fi
+done
